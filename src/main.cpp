635e121313218173eb465491a557712104b334c1
Tuong PV <phamtuongbk999@outlook.com>
2021-07-12 23:12:12 +0700

    test MPU6050 and OLED

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,32 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <Adafruit_MPU6050.h>
+
+
 /************** DEFINED VARIABLES **************/
 /*                                             */
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-float accX = 0;
-float accY = 0;
-float accZ = 0;
+// float accX = 0;
+// float accY = 0;
+// float accZ = 0;
 
-float gyroX = 0;
-float gyroY = 0;
-float gyroZ = 0;
+// float gyroX = 0;
+// float gyroY = 0;
+// float gyroZ = 0;
 
-float temp = 0;
+// float temp = 0;
+
+// uint16_t adcX = 0;
+// uint16_t adcY = 0;
+// uint16_t adcZ = 0;
 
 /*********** COMMUNICATION SELECTION ***********/
 //ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
-MPU6886 imu(MPU6886_ADDRESS, Wire);
+//MPU6886 imu(MPU6886_ADDRESS, Wire);
+Adafruit_MPU6050 mpu;
 
 
 //Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
@@ -36,23 +44,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
 /******************** SETUP ********************/
 /*          Configure ADXL345 Settings         */
 void setup(){
-  uint8_t i_am = 0;
   Serial.begin(115200);                 // Start the serial terminal
-  Serial.println("Let's start learning....");
-  Serial.println();
-  
-  // Wire.begin(21, 22);
-  Wire.begin();
-  MPU6886 imu(MPU6886_ADDRESS, Wire);
+  Serial.println("Let's start...");
 
-
-
-  imu.Init();
-
-  i_am = imu.whoAmI();
-
-  Serial.println(F("WhoAmI() = "));
-  Serial.println(i_am, HEX);
+  if (!mpu.begin()) {
+    Serial.println("Sensor init failed");
+    while (1)
+      yield();
+  }
+  Serial.println("Found a MPU-6050 sensor");
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
@@ -60,41 +60,55 @@ void setup(){
     for (;;)
       ; // Don't proceed, loop forever
   }
+
   display.display();
   delay(500); // Pause for 2 seconds
-  //display.setTextSize(1);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setRotation(0);
-
-  // Clear the buffer
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(1);
-
-  display.println(F("MPU6886..."));
-  display.display();
-  delay(1000);
-
 
 }
 
 /****************** MAIN CODE ******************/
 /*     Accelerometer Readings and Interrupt    */
 void loop(){
-  //display.clearDisplay();
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-  imu.getGyroData(&gyroX,&gyroY,&gyroZ);
-  imu.getAccelData(&accX,&accY,&accZ);
-  imu.getTempData(&temp);
+  display.clearDisplay();
+  display.setCursor(0, 0);
 
-  Serial.println(accX,DEC);
-  Serial.println(accY,DEC);
-  Serial.println(accZ,DEC);
-  Serial.println(temp,10);
-  //printf("\n\r");
-  delay(1000);
-  //display.setCursor(0, 0);
-  //display.printFloat(gyroX, 2);
+  Serial.print("Accelerometer ");
+  Serial.print("X: ");
+  Serial.print(a.acceleration.x, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Y: ");
+  Serial.print(a.acceleration.y, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Z: ");
+  Serial.print(a.acceleration.z, 1);
+  Serial.println(" m/s^2");
 
+  Serial.print("Gyroscope ");
+  Serial.print("X: ");
+  Serial.print(g.gyro.x, 1);
+  Serial.print(" rps, ");
+  Serial.print("Y: ");
+  Serial.print(g.gyro.y, 1);
+  Serial.print(" rps, ");
+  Serial.print("Z: ");
+  Serial.print(g.gyro.z, 1);
+  Serial.println(" rps");
+
+  display.println("Gyroscope - rps");
+  display.print(g.gyro.x, 1);
+  display.print(", ");
+  display.print(g.gyro.y, 1);
+  display.print(", ");
+  display.print(g.gyro.z, 1);
+  display.println("");
+
+  display.display();
+  delay(100);
 }
 
